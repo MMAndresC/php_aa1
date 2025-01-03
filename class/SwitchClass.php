d3b2e64efb2c02f5e14138a4a0b61928c959247e
Mar <mandrescasabon@hotmail.com>
2025-01-03 20:05:39 +0100

    feat: ✨️add switch web

---
+++ class/SwitchClass.php
@@ -0,0 +1,136 @@
+<?php
+
+class SwitchClass
+{
+    public int $num1;
+    public int $num2;
+    public int $num3;
+
+    public string $fruit1;
+    public string $fruit2;
+    public string $fruit3;
+
+    public function __construct(string $fruit1, string $fruit2, string $fruit3, int $num1, int $num2, int $num3) {
+        $this->fruit1 = $fruit1;
+        $this->fruit2 = $fruit2;
+        $this->fruit3 = $fruit3;
+        $this->num1 = $num1;
+        $this->num2 = $num2;
+        $this->num3 = $num3;
+    }
+
+    public function getDescription(): string
+    {
+        return
+            "Descripción\n"
+            . "La estructura de control <strong>switch</strong> es equivalente a varios <strong>if</strong> anidados\n"
+            . "Busca la expresión que cumple y a partir de ahí ejecuta todas las líneas del <strong>switch</strong> hasta encontrar un <strong>break</strong>\n"
+            . "La sentencia <strong>default</strong> al final es equivalente al <strong>else</strong>, entrara unicamente si no cumple ninguno de los <strong>case</strong> \n"
+            . "Se pueden agrupar varios <strong>case</strong> en los que se quiere ejecutar las mismas sentencias de código \n"
+        ;
+    }
+
+    public function getStructure(): string {
+        return
+            "La estructura sera: \n" .
+            "switch(expresión) { \n" .
+            "case opción 1: \n" .
+            "  ...comandos \n" .
+            "break; \n" .
+            "case opción 2: \n" .
+            "  ...comandos \n" .
+            "break; \n" .
+            "case opción 3: \n" .
+            "case opción 4: \n" .
+            "  ...comandos \n" .
+            "break; \n" .
+            "...(Todos los case necesarios)...\n" .
+            "default: {\n" .
+            "  ...comandos \n" .
+            "break; \n" .
+            "}\n"
+            ;
+    }
+
+    public function getAlternativeStructure(): string {
+        return
+            "Sintaxis alternativa:\n" .
+            "switch(expresión): \n" .
+            "case opción 1: \n" .
+            "  ...comandos \n" .
+            "break; \n" .
+            "case opción 2: \n" .
+            "  ...comandos \n" .
+            "break; \n" .
+            "case opción 3: \n" .
+            "case opción 4: \n" .
+            "  ...comandos \n" .
+            "break; \n" .
+            "...(Todos los case necesarios)...\n" .
+            "default: {\n" .
+            "  ...comandos \n" .
+            "break; \n" .
+            "endswitch;\n"
+            ;
+    }
+
+    public function getResultNumbers(): string {
+        $response = "";
+        switch($this->num1) {
+            case $this->num2:
+                $response = "El número es igual al número 2 de valor " . $this->num2;
+                break;
+            case $this->num3:
+                $response = "El número es igual al número 3 de valor " . $this->num3;
+                break;
+            default:
+                $response = "El número no es igual a ninguno de los otros números";
+        }
+        return $response;
+    }
+
+    public function getResultFruits(): string {
+        $response = "";
+        switch($this->fruit1) {
+            case $this->fruit2:
+            case $this->fruit3:
+                $response = "Es una de las dos frutas";
+                break;
+            default:
+                $response = "No es ninguna de las dos frutas";
+        }
+        return $response;
+    }
+
+    //Getters
+    public function getNum1(): int
+    {
+        return $this->num1;
+    }
+
+    public function getNum2(): int
+    {
+        return $this->num2;
+    }
+
+    public function getNum3(): int
+    {
+        return $this->num3;
+    }
+
+    public function getFruit1(): string
+    {
+        return $this->fruit1;
+    }
+
+    public function getFruit2(): string
+    {
+        return $this->fruit2;
+    }
+
+    public function getFruit3(): string
+    {
+        return $this->fruit3;
+    }
+
+}
---
+++ switch.php
@@ -0,0 +1,120 @@
+<?php
+require './class/SwitchClass.php';
+require './utils/utils.php';
+include './partials/header.html';
+
+$num1 = $_POST['num1'] ?? 79;
+$num2 = $_POST['num2'] ?? 42;
+$num3 = $_POST['num3'] ?? 13;
+$fruit1 = $_POST['fruit1'] ?? "Plátano";
+$fruit2 = $_POST['fruit2'] ?? "Cereza";
+$fruit3 = $_POST['fruit3'] ?? "Pera";
+
+$instance = new SwitchClass($fruit1, $fruit2, $fruit3, $num1, $num2, $num3);
+
+?>
+<main>
+    <div>
+        <?php
+        drawLines($instance->getDescription());
+        ?>
+    </div>
+    <div>
+        <?php
+        drawLines($instance->getStructure());
+        ?>
+    </div>
+    <div>
+        <?php
+        drawLines($instance->getAlternativeStructure());
+        ?>
+    </div>
+    <div>
+        <h2>Ejemplos</h2>
+        <div>
+            <div>
+                <div>
+                    <p>switch($num1) {</p>
+                    <p>case $num2:</p>
+                    <p>echo "El número es igual al número 2 de valor " . $num2</p>
+                    <p>break</p>
+                    <p>case $num3:</p>
+                    <p>echo "El número es igual al número 3 de valor " . $num3</p>
+                    <p>break</p>
+                    <p>default:</p>
+                    <p>echo "El número no es igual a ninguno de los otros números"</p>
+                </div>
+            </div>
+
+            <div>
+                <h4>Cambiar números del ejemplo</h4>
+                <form action="switch.php" method="post">
+                    <div>
+                        <label for="num1">Número 1
+                            <input type="text" name="num1" id="num1" value="<?php echo $instance->getNum1() ?>">
+                    </div>
+                    <div>
+                        <label for="num2">Número 2
+                            <input type="text" name="num2" id="num2" value="<?php echo $instance->getNum2() ?>">
+                    </div>
+                    <div>
+                        <label for="num3">Número 3
+                            <input type="text" name="num3" id="num3" value="<?php echo $instance->getNum3() ?>">
+                    </div>
+                    <div>
+                        <input type="submit" value="Cambiar"/>
+                    </div>
+                </form>
+            </div>
+            <div>
+                <h4>Resultado: <?php echo $instance->getResultNumbers()?></h4>
+            </div>
+        </div>
+
+        <div>
+            <div>
+                <div>
+                    <p>switch($fruit1) {</p>
+                    <p>case $fruit2:</p>
+                    <p>case $fruit3:</p>
+                    <p>echo "Es una de las dos frutas"</p>
+                    <p>break</p>
+                    <p>default:</p>
+                    <p>echo "No es ninguna de las dos frutas"</p>
+                    <p>}</p>
+                </div>
+            </div>
+            <div>
+                <h4>Cambiar frutas del ejemplo</h4>
+                <form action="switch.php" method="post">
+                    <div>
+                        <label for="fruit1">Fruta 1
+                            <input type="text" name="fruit1" id="fruit1" value="<?php echo $instance->getFruit1() ?>">
+                    </div>
+                    <div>
+                        <label for="fruit2">Fruta 2
+                            <input type="text" name="fruit2" id="fruit2" value="<?php echo $instance->getFruit2() ?>">
+                    </div>
+                    <div>
+                        <label for="fruit3">Fruta 3
+                            <input type="text" name="fruit3" id="fruit3" value="<?php echo $instance->getFruit3() ?>">
+                    </div>
+                    <div>
+                        <input type="submit" value="Cambiar"/>
+                    </div>
+                </form>
+            </div>
+            <div>
+                <h4>Resultado: <?php echo $instance->getResultFruits()?></h4>
+            </div>
+        </div>
+
+    </div>
+
+
+</main>
+
+<?php
+include './partials/footer.html';
+?>
+
